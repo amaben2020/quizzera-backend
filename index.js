11d96193e29271708a43a9c6e9534d7546381c13
algomachine007 <amaben202phantom@gmail.com>
2024-02-28 22:17:30 +0100

    adding teachers route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./src/app.js";
 
-//sets up a server for the app to run on so that you can view it using localhost:[port]
+//sets up a server for the app to run on so that you can view it using localhost:[port].
 app.listen(5001, () => {
   console.log("running");
 });
