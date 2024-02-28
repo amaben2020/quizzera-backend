6ae842ee935236c2c3f350ad3520a626b290bd13
algomachine007 <amaben202phantom@gmail.com>
2024-02-28 14:18:45 +0100

    adding teachers route

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+// invoke all middlewares here
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
 export default app;
