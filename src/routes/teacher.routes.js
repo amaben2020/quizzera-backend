972e16b64966027226e25cb7137bd65bd8ac4cef
algomachine007 <amaben202phantom@gmail.com>
2024-02-29 07:45:10 +0100

    scaffolding teacher model

--- src/routes/teacher.routes.js
+++ src/routes/teacher.routes.js
@@ -11,12 +11,4 @@ router.route("/").get((req, res, next) => {
   }
 });
 
-router.route("/hey").get((req, res, next) => {
-  try {
-    return res.send("Running");
-  } catch (error) {
-    next("ERROR", error);
-  }
-});
-
 export default router;
